c7417ca03983b0bd3cecd9ee2d69ae94d6548d90
davies368t-rgb <davies368t@gmail.com>
2025-11-28 06:49:45 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+set1 = {5,True,"oadeshbfd",12.52,100,100}
+print(set1)
+
+set1.pop()
+print(set1)
+
+set4 = set([3,4,5,6,7])
+print(set4)
+
+list1 = [1,2,6,4,9,2,10]
+set2 = set(list1)
+print(set2)
+
+print(set4.union(set2))
